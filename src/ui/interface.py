c53a409dc138ac6fc1913c971fe00105be4eb6b3
nonameebyrator985 <singheingemo@hotmail.com>
2025-08-15 20:20:04 +0900

    feat: initial commit for src/ui/interface.py

---
+++ src/ui/interface.py
@@ -0,0 +1,7 @@
+def display_menu():
+    print('1. Ajouter un endpoint')
+    print('2. Lister les endpoints')
+    print('3. Quitter')
+
+    choice = input('Choisissez une option: ')
+    return choice
